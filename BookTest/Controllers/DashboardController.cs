9c7cd35fd8f438693c87460255d9d5fea62530e4
Sally Attalla <eng.sally.atalla@gmail.com>
2023-10-31 11:28:21 +0200

    working  to  Dashboard Part1

---
+++ BookTest/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookTest.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ BookTest/Core/ViewModels/Dashboard/DashboardViewModel.cs
@@ -0,0 +1,12 @@
+﻿using BookTest.Core.ViewModels.Books;
+
+namespace BookTest.Core.ViewModels.Dashboard
+{
+    public class DashboardViewModel
+    {
+        public  int NumberOfCopies { get; set; }
+        public int NumberOfSubscribers { set; get; }
+        public IList<BookDetailsViewModel> LastAddedBooks { get; set; } = new List<BookDetailsViewModel>();
+        public IList<BookDetailsViewModel> TopBooks { get; set; } = new List<BookDetailsViewModel>();
+    }
+}
