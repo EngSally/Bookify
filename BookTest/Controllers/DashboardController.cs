000b77be8e60c95277f1a7ba06a45a7ed9e368b8
Sally Attalla <eng.sally.atalla@gmail.com>
2023-11-05 23:30:56 +0200

    Dashboard part2

--- BookTest/Controllers/DashboardController.cs
+++ BookTest/Controllers/DashboardController.cs
@@ -1,13 +1,132 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BookTest.Core.ViewModels.Dashboard;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BookTest.Controllers
 {
     [Authorize]
     public class DashboardController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper _mapper;
+
+        public DashboardController(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+
         public IActionResult Index()
         {
-            return View();
+            //var numberOfCopies = _context.BookCopies.Count(c => !c.IsDeleted);
+            ////var numberOfCopies = _context.Books.Count(c => !c.IsDeleted);
+
+            //numberOfCopies = numberOfCopies <= 10 ? numberOfCopies : numberOfCopies / 10 * 10;
+
+            //var numberOfsubscribers = _context.Subscribers.Count(c => !c.IsDeleted);
+            //var lastAddedBooks = _context.Books
+            //                    .Include(b => b.Author)
+            //                    .Where(b => !b.IsDeleted)
+            //                    .OrderByDescending(b => b.Id)
+            //                    .Take(8)
+            //                    .ToList();
+
+            //var topBooks = _context.RentalCopies
+            //    .Include(c => c.BookCopy)
+            //    .ThenInclude(c => c!.Book)
+            //    .ThenInclude(b => b!.Author)
+            //    .GroupBy(c => new
+            //    {
+            //        c.BookCopy!.BookId,
+            //        c.BookCopy!.Book!.Title,
+            //        c.BookCopy!.Book!.ImageThumbnailUrl,
+            //        AuthorName = c.BookCopy!.Book!.Author!.Name
+            //    })
+            //    .Select(b => new
+            //    {
+            //        b.Key.BookId,
+            //        b.Key.Title,
+            //        b.Key.ImageThumbnailUrl,
+            //        b.Key.AuthorName,
+            //        Count = b.Count()
+            //    })
+            //    .OrderByDescending(b => b.Count)
+            //    .Take(6)
+            //    .Select(b => new BookViewModel
+            //    {
+            //        Id = b.BookId,
+            //        Title = b.Title,
+            //        ImageThumbnailUrl = b.ImageThumbnailUrl,
+            //        Author = b.AuthorName
+            //    })
+            //    .ToList();
+
+            //var viewModel = new DashboardViewModel
+            //{
+            //    NumberOfCopies = numberOfCopies,
+            //    NumberOfSubscribers = numberOfsubscribers,
+            //    LastAddedBooks = _mapper.Map<IEnumerable<BookViewModel>>(lastAddedBooks),
+            //    TopBooks = topBooks
+            //};
+
+            //return View(viewModel);
+            return Ok();
+        }
+
+        [AjaxOnly]
+        public IActionResult GetRentalsPerDay(DateTime? startDate, DateTime? endDate)
+        {
+            //startDate ??= DateTime.Today.AddDays(-29);
+            //endDate ??= DateTime.Today;
+
+            //var data = _context.RentalCopies
+            //    .Where(c => c.RentalDate >= startDate && c.RentalDate <= endDate)
+            //    .GroupBy(c => new { Date = c.RentalDate })
+            //    .Select(g => new ChartItemViewModel
+            //    {
+            //        Label = g.Key.Date.ToString("d MMM"),
+            //        Value = g.Count().ToString()
+            //    })
+            //    .ToList();
+
+            /*
+			List<ChartItemViewModel> figures = new ();
+
+            for (var day = startDate; day <= endDate; day = day.Value.AddDays(1))
+            {
+                var dayData = data.SingleOrDefault(d => d.Label == day.Value.ToString("d MMM"));
+
+                ChartItemViewModel item = new()
+                {
+                    Label = day.Value.ToString("d MMM"),
+                    Value = dayData is null ? "0" : dayData.Value
+				};
+
+                figures.Add(item);
+            }
+            */
+
+            return Ok();
+        }
+
+        [AjaxOnly]
+        public IActionResult GetSubscribersPerCity()
+        {
+            //var data = _context.Subscribers
+            //    .Include(s => s.Governorate)
+            //    .Where(s => !s.IsDeleted)
+            //    .GroupBy(s => new { GovernorateName = s.Governorate!.Name })
+            //    .Select(g => new ChartItemViewModel
+            //    {
+            //        Label = g.Key.GovernorateName,
+            //        Value = g.Count().ToString()
+            //    })
+            //    .ToList();
+
+            //return Ok(data);
+            return Ok();
         }
     }
+
 }
