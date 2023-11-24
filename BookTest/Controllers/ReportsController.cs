2470f78793f37632a5fcbf47d790a6effcc5087f
Sally Attalla <eng.sally.atalla@gmail.com>
2023-11-24 11:57:09 +0200

    Reports Controller

---
+++ BookTest/Controllers/ReportsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookTest.Controllers
+{
+    public class ReportsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Books() 
+        {
+            return View(); 
+        }
+    }
+}
