0a438e702cec6563e28cf478ab028c878330da48
Sally Attalla <eng.sally.atalla@gmail.com>
2023-12-15 22:07:46 +0200

    Design  Report

--- BookTest/Controllers/ReportsController.cs
+++ BookTest/Controllers/ReportsController.cs
@@ -1,4 +1,7 @@
-﻿using BookTest.Core.ViewModels.Reports;
+﻿using BookTest.Core.Models;
+using BookTest.Core.ViewModels.Reports;
+using ClosedXML.Excel;
+using DocumentFormat.OpenXml.Spreadsheet;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace BookTest.Controllers
@@ -45,6 +48,58 @@ namespace BookTest.Controllers
                 viewModel.Books = PaginatedList<Book>.Create(books, pageNumber ?? 0, (int)ReportsConfigurations.PageSize);
 
             return View(viewModel);
+        }
+        public  async  Task<IActionResult>ExportBooksToExcel(string  authors, string categories)
+        {
+            var selectedAuthors=authors?.Split(',');
+            var selectedCategories=categories?.Split(',');
+
+           var books = _context.Books
+                        .Include(b => b.Author)
+                        .Include(b => b.Categories)
+                        .ThenInclude(c => c.Category)
+                        .Where(b => (string.IsNullOrEmpty(authors) || selectedAuthors.Contains(b.AuthorId.ToString()))
+                        && (string.IsNullOrEmpty(categories)|| b.Categories.Any(c => selectedCategories.Contains(c.CategoryId.ToString()))))
+                        .ToList();
+         using  XLWorkbook workbook = new XLWorkbook();
+            var sheet=workbook.AddWorksheet("Books");
+             
+            sheet.Cell(1, 1).SetValue("Title");
+            sheet.Cell(1, 2).SetValue("Author");
+            sheet.Cell(1, 3).SetValue("Categories");
+            sheet.Cell(1, 4).SetValue("Publisher");
+            sheet.Cell(1, 5).SetValue("Publishing Date");
+            sheet.Cell(1, 6).SetValue("Hall");
+            sheet.Cell(1, 7).SetValue("Available for rental");
+            sheet.Cell(1, 8).SetValue("Status");
+            var header=sheet.Range(1,1,1,8);
+            header.Style.Fill.BackgroundColor = XLColor.Black;
+            header.Style.Font.FontColor = XLColor.White;
+            header.Style.Font.Bold = true;
+
+            for(int i = 0;i < books.Count; i++) 
+            {
+                sheet.Cell(i+2, 1).SetValue(books[i].Title);
+                sheet.Cell(i+2, 2).SetValue(books[i].Author!.Name);
+                sheet.Cell(i+2, 3).SetValue(string.Join(", ", (books[i].Categories!.Select(c => c.Category!.Name))));
+                sheet.Cell(i+2, 4).SetValue(books[i].Publisher);
+                sheet.Cell(i+2, 5).SetValue(books[i].PublishingDate.ToString("d MMM, yyyy"));
+                sheet.Cell(i+2, 6).SetValue(books[i].Hall);
+                sheet.Cell(i+2, 7).SetValue(books[i].IsAvailableForRental ? "Yes" : "No");
+                sheet.Cell(i+2, 8).SetValue(books[i].Deleted ? "Deleted" : "Available");
+            }
+            sheet.ColumnsUsed().AdjustToContents();
+            sheet.Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+            sheet.CellsUsed().Style.Border.OutsideBorder = XLBorderStyleValues.Thin;
+            sheet.Columns("A,C").Style.Fill.BackgroundColor = XLColor.RedRyb;
+
+            sheet.CellsUsed().Style.Border.OutsideBorderColor = XLColor.Red;
+
+
+            await   using  var stream=new MemoryStream();
+
+            workbook.SaveAs(stream);
+            return File(stream.ToArray(), "application/octet-stream", $"Books{DateTime.Today.Date}.xlsx");
         }
 
     }
